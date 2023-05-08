e9af5277f9307fdf9b01b4bad3b9945f77174c1d
Casper Verswijvelt <casperverswijvelt@gmail.com>
2023-05-08 23:39:03 +0200

    Consistent alertdialog background color

--- app/src/main/java/be/casperverswijvelt/unifiedinternetqs/ui/pages/IndividualSettingsPage.kt
+++ app/src/main/java/be/casperverswijvelt/unifiedinternetqs/ui/pages/IndividualSettingsPage.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -29,6 +30,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -117,7 +119,12 @@ fun IndividualSettingsPage(
         AlertDialog(onDismissRequest = {
             requireUnlockDialogOpen = false
         }) {
-            Surface(shape = AlertDialogDefaults.shape) {
+            val color = AlertDialogDefaults.containerColor
+            Surface(
+                shape = AlertDialogDefaults.shape,
+                color = color,
+                tonalElevation = AlertDialogDefaults.TonalElevation,
+            ) {
                 Column(modifier = Modifier.padding(vertical = padding)) {
                     Text(
                         modifier = Modifier
